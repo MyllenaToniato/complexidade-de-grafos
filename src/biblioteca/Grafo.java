78ed90224cb9643bf600e8466ee41fc5aeca6716
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2026-06-17 21:15:08 -0300

    Update Grafo.java

--- src/biblioteca/Grafo.java
+++ src/biblioteca/Grafo.java
@@ -1,11 +1,44 @@
 package biblioteca;
 
-import java.util.ArrayList;
-
 public class Grafo<T> {
-    private ArrayList<Aresta<T>> arestas;
-    private ArrayList<Vertice<T>> vertices;
+    private final ListaEncadeadaArrayList<Vertice<T>> vertices;
 
     public Grafo() {
+        this.vertices = new ListaEncadeadaArrayList<>();
+    }
+
+    // Adiciona um novo vértice ao grafo se ele já não existir (movido de Aresta.java)
+    public Vertice<T> adicionarVertice(T valor) {
+        Vertice<T> existente = obterVertice(valor);
+        if (existente == null) {
+            Vertice<T> novo = new Vertice<>(valor);
+            this.vertices.adicionar(novo);
+            return novo;
+        }
+        return existente;
+    }
+
+    // Procura por um vértice na lista global através do seu valor único (movido de Aresta.java)
+    public Vertice<T> obterVertice(T valor) {
+        for (int i = 0; i < this.vertices.quantidadeNos(); i++) {
+            Vertice<T> v = this.vertices.get(i);
+            if (v.getValor().equals(valor)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    // Interliga dois vértices criando uma aresta ponderada direcionada 
+    public void adicionarAresta(T origem, T destino, float peso) {
+        Vertice<T> verticeOrigem = adicionarVertice(origem);
+        Vertice<T> verticeDestino = adicionarVertice(destino);
+
+        Aresta<T> novaAresta = new Aresta<>(verticeDestino, peso);
+        verticeOrigem.adicionarAresta(novaAresta);
+    }
+
+    public ListaEncadeadaArrayList<Vertice<T>> getVertices() {
+        return vertices;
     }
 }
